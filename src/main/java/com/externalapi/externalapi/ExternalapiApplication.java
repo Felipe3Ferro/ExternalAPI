492f672a25b6917e4cf3e9ed780b8f6de5e496d0
Felipe3Ferro <felipe.nascimento00@edu.pucrs.br>
2022-01-24 15:15:48 -0300

    Trello next

--- src/main/java/com/externalapi/externalapi/ExternalapiApplication.java
+++ src/main/java/com/externalapi/externalapi/ExternalapiApplication.java
@@ -1,8 +1,10 @@
 package com.externalapi.externalapi;
 
-import com.externalapi.externalapi.Models.Trello;
+import java.util.Arrays;
 
-import org.springframework.boot.SpringApplication;
+import com.externalapi.externalapi.Models.Boards;
+import com.externalapi.externalapi.Models.Cards;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
@@ -11,29 +13,73 @@ import org.springframework.web.util.UriComponentsBuilder;
 @SpringBootApplication
 public class ExternalapiApplication {
 
+
+	public static RestTemplate template = new RestTemplate();
+
 	public static void main(String[] args) {
-		// SpringApplication.run(ExternalapiApplication.class, args);	
-		a();
+		// SpringApplication.run(ExternalapiApplication.class, args);
+		// seila("61eb148bf61d443d512751de");	
+		getAllOfBoards();
 	}
 
-	public static void a(){
-		RestTemplate template = new RestTemplate();
+	public static Boards[] getAllOfBoards(){
 	
 	
 		UriComponents uri = UriComponentsBuilder.newInstance()
 			.scheme("https")
-			.host("api.trello.com/")  
+			.host("api.trello.com/")
 			.path("1/members/me/boards")
-			.queryParam("id", "6036d2062891930a423b81c8")
 			.queryParam("key", "4d22d21f5c6e14648a954f215c23a55f")
 			.queryParam("token", "348302ac70e0e10c33a0d89ebaee4194609f992f422c13bb30d70531718efba3")
 			.build();
 	
-		Trello[] entity =  template.getForObject(uri.toString(), Trello[].class);
-		
+		Boards[] entity =  template.getForObject(uri.toString(), Boards[].class);
+
 		for (int i = 0; i < entity.length; i++) {
 			System.out.println(entity[i]);
 		}
+		
+		return entity;
+	}
+	
+	public static Cards[] getListCardsOfBoards(String id){
+
+		//https://api.trello.com/1/boards/61eb148bf61d443d512751de/cards
+		
+		UriComponents uri = UriComponentsBuilder.newInstance()
+			.scheme("https")
+			.host("api.trello.com/")
+			.path("1/boards/")
+			.path(id + "/cards")
+			.queryParam("key", "4d22d21f5c6e14648a954f215c23a55f")
+			.queryParam("token", "348302ac70e0e10c33a0d89ebaee4194609f992f422c13bb30d70531718efba3")
+			.build();
+	
+		Cards[] entity =  template.getForObject(uri.toString(), Cards[].class);
+
+		return entity;
+	}
+
+	public static void seila(String id){
+	
+		Arrays.stream(getListCardsOfBoards(id)).map(cards -> cards.getName()).forEach(System.out::println);
+
+	}
+
+
+	//== == == == == == == == == == == == == == == == == == == == == == == == == ==
+
+	public static void postABoard(){
+
+		UriComponents uri = UriComponentsBuilder.newInstance()
+			.scheme("https")
+			.host("api.trello.com/")
+			.path("1/members/me/boards")
+			.queryParam("name", "Create board test")
+			.queryParam("key", "4d22d21f5c6e14648a954f215c23a55f")
+			.queryParam("token", "348302ac70e0e10c33a0d89ebaee4194609f992f422c13bb30d70531718efba3")
+			.build();
+			
 	}
 
 }
--- src/main/java/com/externalapi/externalapi/controller/Controller.java
+++ src/main/java/com/externalapi/externalapi/controller/Controller.java
@@ -1,6 +1,5 @@
 package com.externalapi.externalapi.controller;
 
-import com.externalapi.externalapi.Models.Todo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,33 +15,33 @@ import java.util.List;
 @RequestMapping("/externalapi")
 public class Controller {
 
-    @GetMapping
-    public List<Todo> todo() {
+    // @GetMapping
+    // public List<Todo> todo() {
 
-        List<String> list = Arrays.asList("61d49225f0a00d1c380d9bdc","61d4947638055f100cdefb7e");
+    //     List<String> list = Arrays.asList("61d49225f0a00d1c380d9bdc","61d4947638055f100cdefb7e");
 
-        // return list.stream().map(t -> {
-        //     return restTemplate.getForObject("http://localhost:8080/v1/todo/" + t , Todo.class);
-        // }).collect(Collectors.toList());
+    //     return list.stream().map(t -> {
+    //         return restTemplate.getForObject("http://localhost:8080/v1/todo/" + t , Todo.class);
+    //     }).collect(Collectors.toList());
             
-        return metodoRestTemplate(list);
+    //     return metodoRestTemplate(list);
         
-        // return Stream.of(Arrays.asList("61d49225f0a00d1c380d9bdc","61d4947638055f100cdefb7e"))
-        //     .map(t -> restTemplate.getForObject("http://localhost:8080/v1/todo/" + t , Todo.class))
-        //     .collect(Collectors.toList());
-    }
+    //     return Stream.of(Arrays.asList("61d49225f0a00d1c380d9bdc","61d4947638055f100cdefb7e"))
+    //         .map(t -> restTemplate.getForObject("http://localhost:8080/v1/todo/" + t , Todo.class))
+    //         .collect(Collectors.toList());
+    // }
 
-    public List<Todo> metodoRestTemplate(List<String> s){
-        List<Todo> a = new ArrayList<>();
-        for (String aux : s) {
-          a.add((new RestTemplate().getForObject("http://localhost:8080/v1/todo/" + aux , Todo.class))); 
-        }
-       return a;
-    }
+    // public List<Todo> metodoRestTemplate(List<String> s){
+    //     List<Todo> a = new ArrayList<>();
+    //     for (String aux : s) {
+    //       a.add((new RestTemplate().getForObject("http://localhost:8080/v1/todo/" + aux , Todo.class))); 
+    //     }
+    //    return a;
+    // }
 
-    @PostMapping
-    public Todo create(@RequestBody Todo todo) {
-       return new RestTemplate().postForObject("http://localhost:8080/v1/todo/", todo, Todo.class);
-    }
+    // @PostMapping
+    // public Todo create(@RequestBody Todo todo) {
+    //    return new RestTemplate().postForObject("http://localhost:8080/v1/todo/", todo, Todo.class);
+    // }
 
 }
